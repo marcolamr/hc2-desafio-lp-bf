d3773438aa9a50eb677e2466381534737226dcf8
Marcos <marcola.mr@gmail.com>
2021-07-17 09:50:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import logo from './logo.png';
+import * as S from './styled';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [registered, setRegistered] = useState(false);
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+      let today = new Date();
+      today.setHours(today.getHours() + 1);
+
+      localStorage.setItem('bflead', JSON.stringify({username, email, date: today.getTime()}));
+      setUsername('');
+      setEmail('');
+      setRegistered(true);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('bflead')) {
+      const data = JSON.parse(localStorage.getItem('bflead'));
+
+      if (data.date > Date.now()) {
+        setUsername(data.username);
+        setEmail(data.email);
+        setRegistered(true);
+      } else {
+        setUsername('');
+        setEmail('');
+        setRegistered(false);
+        localStorage.removeItem('bflead');
+      }
+    }
+  }, []);
+
+  const handleUsernameChange = event => setUsername(event.target.value);
+  const handleEmailChange = event => setEmail(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.Container>
+      <main>
+        <S.Logo>
+          <img src={logo} alt="Logo Black Friday" />
+        </S.Logo>
+        <S.Counter>
+          <div className="headline" id="headline">26.NOV</div>
+          <div id="countdown">
+            <ul>
+              <li><span id="days">134 </span>dias</li>
+              <li><span id="dots">:</span></li>
+              <li><span id="hours">20 </span>horas</li>
+              <li><span id="dots">:</span></li>
+              <li><span id="minutes">15 </span>minutos</li>
+            </ul>
+          </div>
+        </S.Counter>
+      </main>
+      <S.H1 id="principal">Cadastre-se e receba ofertas antecipadas na melhor Black Friday de todos os tempos.</S.H1>
+      <S.H1Mobile id="mobile">Cadastre-se para receber ofertas<br />antes de todo mundo!</S.H1Mobile>
+      <S.Separator className="separator"></S.Separator>
+      <S.Lead>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome:</label>
+          <input type="text" id="name" placeholder="Digite seu nome" value={username} onChange={handleUsernameChange} autoComplete="name" />
+          <label htmlFor="email">E-mail:</label>
+          <input type="email" id="email" placeholder="Digite seu melhor e-mail" value={email} onChange={handleEmailChange} autoComplete="username" inputMode="email" />
+          <button type="submit">CADASTRAR</button>
+        </form>
+        {registered &&
+        <p>Parabéns! Você já se cadastrou com os dados acima. Caso queira, basta alterar e cadastrar novamente</p>
+        }
+      </S.Lead>
+    </S.Container>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,158 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 20px 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & main {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-start;
+    margin-bottom: 20px;
+
+    @media(max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+
+  @media(max-width: 768px) {
+    display: block;
+    padding: 40px 20px;
+    height: auto;
+  }
+`;
+
+export const Logo = styled.div`
+  & img {
+    max-width: 350px;
+
+    @media(max-width: 768px) {
+      margin-bottom: 20px
+    }
+  }
+`;
+
+export const Counter = styled.div`
+  & .headline {
+    height: 70px;
+    background-color: var(--secondary-color);
+    font-size: 3rem;
+    font-weight: bold;
+    margin-bottom: 20px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & li {
+    display: inline-block;
+    font-size: 1em;
+    list-style-type: none;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  & li span {
+    display: block;
+    font-size: 4rem;
+  }
+
+  & li span#dots {
+    margin: 0 20px;
+  }
+`;
+
+export const H1 = styled.h1`
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+
+  @media(max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const H1Mobile = styled.h1`
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: center;
+  display: none;
+
+  @media(max-width: 768px) {
+    display: block;
+  }
+`;
+
+export const Separator = styled.div`
+  margin: 40px 0;
+  width: 100%;
+  height: 1px;
+  background-color: var(--secondary-color);
+`;
+
+export const Lead = styled.div`
+  & form {
+    @media(max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  
+  & form label {
+    display: none;
+  }
+
+  & form input {
+    width: 350px;
+    height: 45px;
+    border-radius: 3px;
+    padding: 0 15px;
+    color: var(--secondary-color);
+    margin-right: 10px;
+
+    @media(max-width: 768px) {
+      margin: 0;
+      width: 85%;
+
+      &:not(:first-of-type) {
+        margin-top: 10px;
+      }
+    }
+  }
+
+  & form button[type=submit] {
+    width: 200px;
+    height: 45px;
+    border-radius: 3px;
+    color: var(--primary-color);
+    background-color: var(--secondary-color);
+    cursor: pointer;
+    font-size: 1rem;
+    opacity: 1;
+    transition: opacity 0.5s;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    @media(max-width: 768px) {
+      width: 85%;
+      margin-top: 10px;
+    }
+  }
+
+  & p {
+    margin-top: 10px;
+    font-size: 0.75rem;
+  }
+`;
